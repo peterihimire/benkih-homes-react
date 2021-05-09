cfbfa23c8bb39098a3182527e604e76c665dad37
Peter Ihimire <peterihimire@gmail.com>
2021-05-09 17:14:18 +0100

    Added _redirects file for netlify, added properties and users components, modified the readme file, app file and its css file same with the index file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div id="page-container" className="App">
       <div id="content-wrapper">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
         <h1>Lets get to work Q</h1>
         <p>This is the real application</p>
       </div>
