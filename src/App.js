b22390bf37de36fdc2baaf5df615e25135fa8135
Peter Ihimire <peterihimire@gmail.com>
2021-05-09 16:01:32 +0100

    Still working on the structure of my property app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Lets get to work Q</h1>
-     <p>This is the real application</p>
+    <div id="page-container" className="App">
+      <div id="content-wrapper">
+        <h1>Lets get to work Q</h1>
+        <p>This is the real application</p>
+      </div>
+
+      <footer>
+        <p>copyright &copy; of benkih-homes 2021</p>
+      </footer>
     </div>
   );
 }
