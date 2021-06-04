f22d6f27c51bda43edeb244ad928467cb4997727
Peter Ihimire <peterihimire@gmail.com>
2021-06-04 11:02:23 +0100

    Working on the contact-item form

--- src/properties/components/ContactItem.js
+++ src/properties/components/ContactItem.js
@@ -6,6 +6,28 @@ const ContactItem = () => {
   return (
     <div className="contact-container">
       <div className="contact-left">
+        <div className="customer-resource-container">
+          <div className="customer-resource-icon">
+            <FaDesktop className="home-resource-icon" />
+          </div>
+          <div className="customer-resource-text">
+            <h4>customer service contact information:</h4>
+            <div className="customer-resource-contacts">
+              <p>
+                estate.com : <br /> (+234) 813-864-7368 <br />
+                customercare@estate.com
+              </p>
+              <p>
+                new homes : <br /> (+234) 813-864-7368 <br />
+                newhomes@estate.com
+              </p>
+              <p>
+                rentals : <br /> (+234) 813-864-7368 <br />
+                estaterentals@estate.com
+              </p>
+            </div>
+          </div>
+        </div>
         <div className="contact-resource-container">
           <div className="contact-resource-title">
             <h4>
@@ -40,28 +62,6 @@ const ContactItem = () => {
             </div>
           </div>
         </div>
-        <div className="customer-resource-container">
-          <div className="customer-resource-icon">
-            <FaDesktop className="home-resource-icon" />
-          </div>
-          <div className="customer-resource-text">
-            <h4>customer service contact information:</h4>
-            <div className="customer-resource-contacts">
-              <p>
-                estate.com : <br /> (+234) 813-864-7368 <br />
-                customercare@estate.com
-              </p>
-              <p>
-                new homes : <br /> (+234) 813-864-7368 <br />
-                newhomes@estate.com
-              </p>
-              <p>
-                rentals : <br /> (+234) 813-864-7368 <br />
-                estaterentals@estate.com
-              </p>
-            </div>
-          </div>
-        </div>
       </div>
       <aside className="contact-right">
         <div className="contact-form-container">
@@ -71,17 +71,49 @@ const ContactItem = () => {
             please fill out the information below.
           </p>
           <form className="contact-form">
-            <div className="">
-              <input type="text" placeholder="Full Name" />
+            <div className="form-group">
+              <input
+                name="name"
+                type="text"
+                placeholder="Full Name"
+                className="form-field"
+                id="name"
+                value=""
+                onChange={(e) => console.log(e)}
+              />
             </div>
-            <div className="">
-              <input type="email" placeholder="Email" />
+            <div className="form-group">
+              <input
+                name="email"
+                type="email"
+                placeholder="Email"
+                className="form-field"
+                value=""
+                id="email"
+                onChange={(e) => console.log(e)}
+              />
             </div>
-            <div className="">
-              <input type="text" placeholder="Subject" />
+            <div className="form-group">
+              <input
+                name="subject"
+                type="text"
+                placeholder="Subject"
+                className="form-field"
+                value=""
+                id="subject"
+                onChange={(e) => console.log(e)}
+              />
             </div>
-            <div className="">
-              <textarea placeholder="Message"></textarea>
+            <div className="form-group">
+              <textarea
+                name="message"
+                placeholder="Message"
+                className="form-field-textarea"
+                id="message"
+                value=""
+                onChange={(e) => console.log(e)}
+                rows="4"
+              ></textarea>
             </div>
             <button type="submit" className="btn">
               Submit
