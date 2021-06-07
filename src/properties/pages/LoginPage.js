c07b5c6a173e2675728dcd202925cada9fe26a4b
Peter Ihimire <peterihimire@gmail.com>
2021-06-07 07:52:30 +0100

    Working on the loginpage form-validation

--- src/properties/pages/LoginPage.js
+++ src/properties/pages/LoginPage.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 // import LoginItem from "../../properties/components/LoginItem";
 import "./LoginPage.css";
 import homeBg from "../../assets/full-modal.svg";
 import { Link } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 
-const LoginPage = (props) => {
-  console.log(props);
+class LoginPage extends Component {
+  // console.log(props);
+
   // const ValidationMessage = (props) => {
   //   if (!props.valid) {
   //     return <div className="error-msg">{props.message}</div>;
@@ -24,78 +25,93 @@ const LoginPage = (props) => {
   // const [error, setError] = React.useState(null);
 
   // const validateForm = () => {
-  //   setFormValid(() => {
+  //   setFormValid(emailValid && passwordValid);
+  // };
 
-  //   })
-  // }
+  // const updateEmail = (email) => {};
 
-  return (
-   
-    <div className="auth-item">
-      <div className="login-bg-div hidden-xs visible-md visible-xl">
-        <img src={homeBg} alt="home" />
-      </div>
-      <div className="login-form">
-        <div className="login-form-content">
-          <div className="back-arrow">
-            <button className="back-btn" onClick={() => props.history.goBack()}>
-              <FaArrowLeft className="arrow-back-icon" />
-            </button>
+  state = {
+    email: "",
+    emailValid: false,
+    password: "",
+    passwordValid: false,
+    formValid: false,
+    errorMsg: {},
+    loading: false,
+    error: null,
+  };
+
+  render() {
+    return (
+      <div className="auth-item">
+        <div className="login-bg-div hidden-xs visible-md visible-xl">
+          <img src={homeBg} alt="home" />
+        </div>
+        <div className="login-form">
+          <div className="login-form-content">
+            <div className="back-arrow">
+              <button
+                className="back-btn"
+                onClick={() => this.props.history.goBack()}
+              >
+                <FaArrowLeft className="arrow-back-icon" />
+              </button>
+            </div>
+            <div className="heading">
+              <h2>Login to your account</h2>
+              <p>Access all your saved properties, searches, notes and more.</p>
+            </div>
+            <form>
+              <div className="form-group">
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  className="form-field"
+                  value=""
+                  id="email"
+                  onChange={(e) => console.log(e)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  className="form-field"
+                  id="password"
+                  value=""
+                  onChange={(e) => console.log(e)}
+                />
+              </div>
+              <div className="forgot-password-div">
+                <p>
+                  <span>
+                    <Link to="/forgot-password" className="alt-auth">
+                      <b> Forgot your password ?</b>
+                    </Link>
+                  </span>
+                </p>
+              </div>
+              <button type="submit" className="btn btn-full">
+                Login
+              </button>
+              <div className="alt-auth-div">
+                <p>
+                  Don't have an account ?
+                  <span>
+                    <Link to="/register" className="alt-auth">
+                      <b> Sign Up</b>
+                    </Link>
+                  </span>
+                </p>
+              </div>
+            </form>
           </div>
-          <div className="heading">
-            <h2>Login to your account</h2>
-            <p>Access all your saved properties, searches, notes and more.</p>
-          </div>
-          <form>
-            <div className="form-group">
-              <input
-                name="email"
-                type="email"
-                placeholder="Email"
-                className="form-field"
-                value=""
-                id="email"
-                onChange={(e) => console.log(e)}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                name="password"
-                type="password"
-                placeholder="Password"
-                className="form-field"
-                id="password"
-                value=""
-                onChange={(e) => console.log(e)}
-              />
-            </div>
-            <div className="forgot-password-div">
-              <p>
-                <span>
-                  <Link to="/forgot-password" className="alt-auth">
-                    <b> Forgot your password ?</b>
-                  </Link>
-                </span>
-              </p>
-            </div>
-            <button type="submit" className="btn btn-full">
-              Login
-            </button>
-            <div className="alt-auth-div">
-              <p>
-                Don't have an account ?
-                <span>
-                  <Link to="/register" className="alt-auth">
-                    <b> Sign Up</b>
-                  </Link>
-                </span>
-              </p>
-            </div>
-          </form>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default LoginPage;
