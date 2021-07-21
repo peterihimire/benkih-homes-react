d8a7f2d0d767a7d6ab8fde3a9fe335553880648e
Peter Ihimire <peterihimire@gmail.com>
2021-07-21 11:03:06 +0100

    Banner get-started button ranamed to add-property

--- src/shared/components/Banner.js
+++ src/shared/components/Banner.js
@@ -12,11 +12,11 @@ const Banner = () => {
             <p>
               Have you ever thought about investing your hard-earned money on
               something worth it. Invest in our real-estate plan , ranging from
-              as low as 50 thousand to 100 million naira, and be sure to get 10% RIO .
-              To get started, hit the get started button.
+              as low as 50 thousand to 100 million naira, and be sure to get 10%
+              RIO . To get started, hit the get started button.
             </p>
             <button className="btn btn-bigger">
-              <Link to="/signup">Get Started</Link>
+              <Link to="/property/new">add property</Link>
             </button>
           </div>
         </div>
